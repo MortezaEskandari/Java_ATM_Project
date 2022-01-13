4362755f3e6b48b78457c43b6ae80e572cfb8e1f
MortezaEskandari <Mortezaesk1@gmail.com>
2022-01-13 17:50:59 -0500

    Update Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -18,7 +18,7 @@ public class Transaction {
 	private Date timestamp;
 	
 	/*
-	 *  Memo for this transaction.
+	 *  Optional memo for this transaction, if there is one. Empty String "" means no memo.
 	 */
 	private String memo;
 	
@@ -77,18 +77,43 @@ public class Transaction {
 		}
 	}
 	
+	/**
+	 * Getter method for getting the amount this transaction was. The amount withdrew, deposited, or transferred.
+	 * @return	this.amount (double)
+	 */
 	public double getAmount() {
 		return this.amount;
 	}
 	
+	/**
+	 * Getter method for getting the type of transaction that was performed (withdraw, deposit, transfer)
+	 * @return	this.type (String)
+	 */
 	public String getType() {
 		return this.type;
 	}
 	
+	/**
+	 * Getter method for getting the optional memo for this transaction if there was one, empty string "" means no memo.
+	 * @return	this.memo (String)
+	 */
 	public String getMemo() {
 		return this.memo;
 	}
+	
+	/**
+	 * Getter method for getting the value of the old balance before this transaction was performed.
+	 * @return	this.oldBalance (double)
+	 */
 	public double getOldBalance() {
 		return this.oldBalance;
+	}
+	
+	/**
+	 * Getter method for getting the Account object that this transaction was made in
+	 * @return	this.account (Account object)
+	 */
+	public Account getAccount() {
+		return this.account;
 	}
 }
